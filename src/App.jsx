84d12d076ea163da316472e2a8fac80bf9b06a18
davidgit3000 <davidkarik5@gmail.com>
2024-08-07 21:19:50 -0700

    added Lexical demo dev

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+
+// import ExampleTheme from "./ExampleTheme";
+import ToolbarPlugin from "./plugins/ToolbarPlugin.jsx";
+import TreeViewPlugin from "./plugins/TreeViewPlugin.jsx";
+
+const placeholder = "Enter some rich text...";
+
+const theme = {
+  code: "editor-code",
+  heading: {
+    h1: "editor-heading-h1",
+    h2: "editor-heading-h2",
+    h3: "editor-heading-h3",
+    h4: "editor-heading-h4",
+    h5: "editor-heading-h5",
+  },
+  image: "editor-image",
+  link: "editor-link",
+  list: {
+    listitem: "editor-listitem",
+    nested: {
+      listitem: "editor-nested-listitem",
+    },
+    ol: "editor-list-ol",
+    ul: "editor-list-ul",
+  },
+  ltr: "ltr",
+  paragraph: "editor-paragraph",
+  placeholder: "editor-placeholder",
+  quote: "editor-quote",
+  rtl: "rtl",
+  text: {
+    bold: "editor-text-bold",
+    code: "editor-text-code",
+    hashtag: "editor-text-hashtag",
+    italic: "editor-text-italic",
+    overflowed: "editor-text-overflowed",
+    strikethrough: "editor-text-strikethrough",
+    underline: "editor-text-underline",
+    underlineStrikethrough: "editor-text-underlineStrikethrough",
+  },
+};
+
+const editorConfig = {
+  namespace: "Lexical Demo",
+  nodes: [],
+  // Handling of errors during update
+  onError(error) {
+    console.error(error);
+  },
+  // The editor theme
+  theme,
+};
+
+export default function App() {
+  return (
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className="editor-container">
+        <ToolbarPlugin />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className="editor-input"
+                aria-placeholder={placeholder}
+                placeholder={
+                  <div className="editor-placeholder">{placeholder}</div>
+                }
+              />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+          <TreeViewPlugin />
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+}
